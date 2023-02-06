5b53c45b926cf799f1f5f271d2b0e32840cde8d1
Satyam gupta <73402131+satyam9721@users.noreply.github.com>
2023-02-06 11:59:30 +0530

    Add files via upload

---
+++ final.py
@@ -0,0 +1,31 @@
+import json
+from flask import Flask, request, jsonify
+from flask_mongoengine import MongoEngine 
+ 
+app = Flask(__name__)
+app.config['MONGODB_SETTINGS'] = {
+    'db': 'dbtestmongo',
+    'host': 'localhost',
+    'port': 27017
+}
+db = MongoEngine()
+db.init_app(app)
+ 
+class User(db.Document):
+    name = db.StringField()
+    email = db.StringField()
+    def to_json(self):
+        return {"name": self.name,
+                "email": self.email}
+ 
+@app.route('/')
+def query_records():
+    name = 'Satyamgupta'
+    user = User.objects(name=name).first()
+    if not user:
+        return jsonify({'error': 'data not found'})
+    else:
+        return jsonify(user.to_json())
+		
+if __name__ == "__main__":
+    app.run(debug=True)		
